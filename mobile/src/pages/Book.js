168b2b19b0adfdd8124b649f03f866cdda33020e
willrmachado <willian.rodrigues.machado@hotmail.com>
2019-10-23 10:18:48 -0200

    aula 3.5

---
+++ mobile/src/pages/Book.js
@@ -0,0 +1,9 @@
+import React from 'react-native'
+import {View, Text} from 'react-native'
+
+
+export default function Book(){
+    return <View>
+    <Text> olaaaa </Text>
+    </View>
+}
---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react-native'
+import {View, Text} from 'react-native'
+
+
+export default function Login(){
+    return <View>
+    <Text> olaaaa </Text>
+    </View>
+}
