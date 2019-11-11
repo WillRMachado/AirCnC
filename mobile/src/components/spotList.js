4ae119ad90308a54825a3d9bb1479f5b9572bacf
willrmachado <willian.rodrigues.machado@hotmail.com>
2019-11-11 04:55:18 -0200

    1 h 10

--- mobile/src/components/spotList.js
+++ mobile/src/components/spotList.js
@@ -1,27 +1,62 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
 import api from '../services/api'
 
-export default function SpotList({tech}) {
+export default function SpotList({ tech }) {
+
+    const [spots, setSpots] = useState([])
 
 
     useEffect(() => {
         const techAjustada = tech.toLowerCase()
         async function loadSpots() {
             const response = await api.get('./spots', {
-                params:techAjustada
+                params: { "tech": techAjustada }
             })
-            console.log("will")
-            console.log(response.data)
+            setSpots(response.data)
         }
         loadSpots()
     }, [])
 
 
     return (
+        <View styles={styles.container}>
+            {/* <Text>{tech}</Text> */}
+            <Text styles={styles.title}>Empresas que usam {tech} </Text>
+            {/* <Text>{spots}</Text> */}
 
-        <Text>{tech}</Text>
+
+            <FlatList
+            styles={styles.list}
+            data={spots}
+            keyExtractor={spot=>spot._id}
+            horizontal
+            showsHorizontalScrollIndicator ={false}
+            renderItem={({item})=>(
+                <View styles={styles.listItem}>
+                    <Image style = {styles.thumbanil} source={{uri:item.thumbnail_url}}></Image>
+                    <Text>{item.company}</Text>
+                    <Text>{item.price ? `R$:${item.price}/dia` : "GRATUITO"}</Text>
+                </View>
+            )}
+            >
+
+            </FlatList>
+        </View>
     );
 }
 
 
+const styles = StyleSheet.create({
+
+    // container: {
+    //     marginTop: 30,
+    // },
+
+    title: {
+        fontSize: 20,
+        color: "#444",
+        paddingHorizontal: 20,
+        marginBottom: 15,
+    },
+})
