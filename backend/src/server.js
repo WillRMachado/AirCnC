00bf5b573cd99cab62b5ed754141b4d4f593d2f5
willrmachado <willian.rodrigues.machado@hotmail.com>
2019-11-20 05:10:32 -0200

    aula 5 --- 15 min

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,39 @@ const mongoose = require('mongoose')
 const routes = require('./routes')
 const cors = require('cors')
 const path = require('path')
+const socketio = require('socket.io')
+const http = require('http')
 
 
 const app = express()
 
-mongoose.connect('mongodb+srv://AirCnC:HG7ThW1huKrXM0Dh@aircnc-ktl3m.mongodb.net/AirCnCDb?retryWrites=true&w=majority',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+const server = http.Server(app)
+const io = socketio(server)
+
+const connectedUsers = {}
+
+mongoose.connect('mongodb+srv://AirCnC:HG7ThW1huKrXM0Dh@aircnc-ktl3m.mongodb.net/AirCnCDb?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+
+
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    connectedUsers[user_id] = socket.id
+    // socket.on('listerner', data => console.log(data))
+})
+
+
+app.use((req, res, next)=> {
+    req.io = io
+    req.connectedUsers = connectedUsers
+    return next();
+})
+
+
+
 
 
 //req.query => acessar query params (para filtros)(get)
@@ -21,7 +46,7 @@ mongoose.connect('mongodb+srv://AirCnC:HG7ThW1huKrXM0Dh@aircnc-ktl3m.mongodb.net
 //na URL => /minha/url/aqui:id  (id é um route param e vem da url que esta tentando me acessar)
 app.use(cors())
 app.use(express.json())
-app.use('/files',express.static(path.resolve(__dirname,'..','uploads')))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,40 +1,53 @@
-import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import socketio from 'socket.io-client'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
-import  './styles.css'
+import './styles.css'
 
-export default function Dashboard(){
+export default function Dashboard() {
     const [spots, setSpots] = useState([])
 
+    useEffect(() => {
+        const user_id = localStorage.getItem('user')
+        const socket = socketio('http://localhost:3333', {
+            query: { user_id }
+        });
 
-    useEffect(()=>{
-        async function loadSpots(){
+
+        socket.on('booking_request', data=>{window.alert(data)})
+    }, [])
+
+
+    useEffect(() => {
+        async function loadSpots() {
             const user_id = localStorage.getItem('user')
-            const response = await api.get('/dashboard',{
-                headers:{user_id}
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
             })
             setSpots(response.data)
             console.log(response.data)
         }
         loadSpots()
 
-    },[])
-return (
+    }, [])
+
+
+    return (
         <>
             <ul className="spot-list">
-                {spots.map(spot=>(
+                {spots.map(spot => (
                     <li key={spot._id}>
-                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})`}}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}>
 
                         </header>
                         <strong>{spot.company}</strong>
-                        <span>{spot.price ? "R$"+spot.price+"/dia" : "GRATUITO"}</span>
+                        <span>{spot.price ? "R$" + spot.price + "/dia" : "GRATUITO"}</span>
                     </li>
                 ))}
             </ul>
 
-            <Link to = "/new">
-                <button className="btn">Cadastrar novo spot</button> 
+            <Link to="/new">
+                <button className="btn">Cadastrar novo spot</button>
             </Link>
 
         </>
